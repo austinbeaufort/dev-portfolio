31a8e8accd4d339e3c79a054483f3c87da91cfba
Austin Beaufort <tinyhatcoding@gmail.com>
2019-05-20 17:19:10 -0400

    Updates

---
+++ precache-manifest.97c7de5131da479a8bcb788717cded73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d71dbaffeaf68acf89dae56c8a86fcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebff34b76e8094df1dd9",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "32634c4f12422f84a72b",
+    "url": "/static/css/main.0b09b446.chunk.css"
+  },
+  {
+    "revision": "ebff34b76e8094df1dd9",
+    "url": "/static/js/2.218e45e8.chunk.js"
+  },
+  {
+    "revision": "32634c4f12422f84a72b",
+    "url": "/static/js/main.e17ba288.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
