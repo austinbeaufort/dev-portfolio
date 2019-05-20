185fc9373521a1e4349a60190d49e85c52c239a9
Austin Beaufort <tinyhatcoding@gmail.com>
2019-05-20 17:27:17 -0400

    preparing for deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter, Route } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
@@ -11,12 +10,10 @@ class App extends Component {
   render() {
     return (
       <HashRouter basename="/">
-        <CssBaseline>
           <div className="App">
             <Route path='/' exact strict component={Home} />
             <Route path='/contact' exact strict component={Contact} />
           </div>
-        </CssBaseline>
       </HashRouter>
     );
   }
